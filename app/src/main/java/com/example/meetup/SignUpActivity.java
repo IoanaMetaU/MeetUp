0c2433a56ed61281da0538319f86966ebfe58ac2
ioanam25 <ioanam@princeton.edu>
2022-06-21 13:28:36 -0700

    implemented login, logout, signup and google signin

---
+++ app/src/main/java/com/example/meetup/SignUpActivity.java
@@ -0,0 +1,101 @@
+package com.example.meetup;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.textfield.TextInputEditText;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ImageView back;
+    private Button signUp;
+    private TextInputEditText username;
+    private TextInputEditText password;
+    private TextInputEditText passwordAgain;
+    private ProgressDialog progressDialog;
+    private GoogleSignInAccount account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        progressDialog = new ProgressDialog(SignUpActivity.this);
+
+        back = findViewById(R.id.back);
+        signUp = findViewById(R.id.signup);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        passwordAgain = findViewById(R.id.passwordagain);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            account = intent.getParcelableExtra("account");
+            if (account != null)
+                username.setText(account.getDisplayName());
+        }
+
+        signUp.setOnClickListener(v -> {
+            if (password.getText().toString().equals(passwordAgain.getText().toString()) && !TextUtils.isEmpty(username.getText().toString()))
+                signUp(username.getText().toString(), password.getText().toString());
+            else
+                Toast.makeText(this, "Make sure that the values you entered are correct.", Toast.LENGTH_SHORT).show();
+        });
+
+        back.setOnClickListener(v -> finish());
+
+    }
+
+    private void signUp(String username, String password) {
+        progressDialog.show();
+        ParseUser user = new ParseUser();
+        // Set the user's username and password, which can be obtained by a forms
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(e -> {
+            progressDialog.dismiss();
+            if (e == null) {
+                showAlert("Successful Sign Up ! You logged in...\n", "Welcome " + username + " !");
+            } else {
+                ParseUser.logOut();
+                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private void showAlert(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        // don't forget to change the line below with the names of your Activities
+                        Intent intent = new Intent(SignUpActivity.this, LogoutActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+    }
+}
