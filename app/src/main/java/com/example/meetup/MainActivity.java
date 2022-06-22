698420bafeed7a878d3055a0197fb113007ad457
ioanam25 <ioanam@princeton.edu>
2022-06-22 10:59:53 -0700

    created empty fragments

--- app/src/main/java/com/example/meetup/MainActivity.java
+++ app/src/main/java/com/example/meetup/MainActivity.java
@@ -1,29 +1,54 @@
 package com.example.meetup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseObject;
 
-// TODO main activity in progress
 public class MainActivity extends AppCompatActivity {
-
-    private final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
         setContentView(R.layout.activity_main);
-        ParseObject firstObject = new  ParseObject("FirstClass");
-        firstObject.put("message","Hey ! First message from android. Parse is now connected");
-        firstObject.saveInBackground(e -> {
-            if (e != null){
-                Log.e(TAG, e.getLocalizedMessage());
-            }else{
-                Log.d(TAG,"Object saved.");
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+//                switch (item.getItemId()) {
+//                    case R.id.action_home:
+//                        fragment = new FeedFragment();
+//                        break;
+//                    case R.id.action_compose:
+//                        fragment = new ComposeFragment();
+//                        break;
+//                    case R.id.action_maps:
+//                        fragment = new MapsFragment();
+//                        break;
+//                    case R.id.action_search:
+//                        fragment = new SearchFragment();
+//                        break;
+//                    default: fragment = new SearchFragment();
+//                }
+//                fragmentManager.beginTransaction().replace(R.id.fragmentPlaceholder, fragment).commit();
+                return true;
             }
         });
+
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
+
 }
